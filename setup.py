49caf8a5eeaac8d4092298c561f29a3054782194
Alex.Holkner <Alex.Holkner@aa51f1b6-e21a-0410-9ad4-b77617c82e60>
2006-08-14 16:36:11 +0000

    setup.py for registering with PyPI

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+'''
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '$Id$'
+
+from distutils.core import setup
+
+setup(name='euclid',
+      version='0.01',
+      description='2D and 3D vector, matrix, quaternion and geometry module',
+      author='Alex Holkner',
+      author_email='alex@partiallydisassembled.net',
+      url='http://partiallydisassembled.net/euclid.html',
+      py_modules=['euclid'],
+      )
+
