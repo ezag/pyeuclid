dad38410475d101b1f71a6856519e1dc7a240c98
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2016-09-16 01:04:25 +0300

    Code style fixes

--- tests/test_matrix_legacy.py
+++ tests/test_matrix_legacy.py
@@ -38,11 +38,11 @@ def test_m3inv():
         """Assert that matrix is a unity"""
         epsilon = 1e-8
         for s in 'afk':
-            assert(abs(m.__getattribute__(s)-1.0)<epsilon)
+            assert abs(m.__getattribute__(s) - 1.0) < epsilon
         for s in 'bcegij':
-            assert(abs(m.__getattribute__(s))<epsilon)
+            assert abs(m.__getattribute__(s)) < epsilon
 
     # Create a "random" matrix and calculate its inverse
-    m = euclid.Matrix3().rotate(1.0).translate(3,4).scale(1.5,0.7)
+    m = euclid.Matrix3().rotate(1.0).translate(3, 4).scale(1.5, 0.7)
     minv = m.inverse()
-    assert_m3_unity(minv*m)
+    assert_m3_unity(minv * m)
