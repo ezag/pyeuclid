b16ab9cfdd3981a6e9d9b5b0f4750c417e71446f
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2016-09-15 22:55:27 +0300

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
-#!/usr/bin/env python
+from setuptools import find_packages, setup
 
-'''
-'''
+setup(
+    name='euclid',
+    version='1.0.dev0',
+    packages=find_packages(),
 
-__docformat__ = 'restructuredtext'
-__version__ = '$Id$'
-
-from distutils.core import setup
-
-setup(name='euclid',
-      version='0.01',
-      description='2D and 3D vector, matrix, quaternion and geometry module',
-      author='Alex Holkner',
-      author_email='alex@partiallydisassembled.net',
-      url='http://partiallydisassembled.net/euclid.html',
-      py_modules=['euclid'],
-      classifiers = [
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3",
-          "Development Status :: 4 - Beta",
-          "Environment :: Other Environment",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
-          "Operating System :: OS Independent",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          "Topic :: Scientific/Engineering :: Mathematics",
-          ],
-      )
-
+    author='Eugen Zagorodniy',
+    author_email='e.zagorodniy@gmail.com',
+    description='2D and 3D vector, matrix, quaternion and geometry module',
+    license='LGPLv2+',
+    keywords=['geometry'],
+    url='https://github.com/ezag/pyeuclid',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        ('License :: OSI Approved :: '
+         'GNU Lesser General Public License v2 or later (LGPLv2+)'),
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+)
