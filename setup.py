0fbd58822e5d8997b27749146b9623cdee790eb3
Franz Steinmetz <mail@franzsteinmetz.de>
2016-09-22 11:24:37 +0200

    Fix setup.py to install euclid.py
    Installing this package with setuptools leaves out `euclid.py`. Thus after the installation, you cannot do `import euclid`. This is due to an erroneous `setup.py` which is fixed in this PR. The solution came from: http://www.agapow.net/programming/python/setuptools-and-the-single-file/

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='euclid',
     version='1.0.dev0',
-    packages=find_packages(),
+    
+    py_modules=['euclid'],
 
     setup_requires=[
         'pytest-runner',
